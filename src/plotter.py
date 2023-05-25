26de8fbb537509d06648cf960919c06bb8dc0dd1
Elis <67229567+GUNELIS@users.noreply.github.com>
2023-05-25 04:14:07 +0200

    plooter and prep

---
+++ src/plotter.py
@@ -0,0 +1,183 @@
+import Point
+import datetime
+import pandas as pd
+import folium
+from IPython.display import display
+from math import radians, cos, sin, asin, sqrt
+import folium.plugins as plugins
+from folium.plugins import MousePosition
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from scipy import stats
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, roc_curve, roc_auc_score, precision_recall_curve
+from sklearn.inspection import permutation_importance
+
+
+color_list =  ['cadetblue','beige', 'darkred','black', 'orange','darkpurple', 'pink', 'blue', 'darkblue', 'darkgreen', 'gray', 'green', 'lightblue', 'lightgray', 'lightgreen', 'lightred', 'purple', 'red', 'white']
+freq_visited_coordinates = [
+    (27.07892, -79.231),
+    (24.967, -80.940),
+    (24.52352, -82.78676),
+    (26.20509, -82.05591),
+    (27.90888, -84.174475),
+    (29.2778, -84.163),
+    (30.32088, -86.5547),
+    (28.06455, -94.51558),
+    (28.36448, -91.68008)
+]
+
+def mark_map(map,data, the_icon ='lion',color='blue'):
+    mean_lat = data['latitude'].mean()
+    mean_lon = data['longitude'].mean()
+    for i in range(data.shape[0]):
+        lat = data['latitude'].iloc[i]
+        lon = data['longitude'].iloc[i]
+        date2 = data['date'].iloc[i]
+        name = data['name'].iloc[i]        
+        folium.Marker([lat, lon], popup = [name, date2], icon=folium.Icon(color=color,icon=the_icon,prefix='fa')).add_to(map)
+    MousePosition().add_to(map)
+    return map
+
+
+def mark_layered_map(map,h_name,data,the_icon ='lion',color='blue'):
+    group = folium.FeatureGroup(name = h_name)
+    mean_lat = data['latitude'].mean()
+    mean_lon = data['longitude'].mean()
+    for i in range(data.shape[0]):
+        lat = data['latitude'].iloc[i]
+        lon = data['longitude'].iloc[i]
+        date2 = data['date'].iloc[i]
+        name = data['name'].iloc[i]        
+        folium.Marker([lat, lon], popup = [name, date2], icon=folium.Icon(color=color,icon=the_icon,prefix='fa')).add_to(group)
+    group.add_to(map)
+ 
+    MousePosition().add_to(map)
+
+    return map
+
+def mark_before_during_after(map,name,before,during,after):
+    if not before.empty:
+        group_before = folium.FeatureGroup(name = f"Indidvidual {name} Trajectory before event").add_to(map)
+        # line_before = folium.PolyLine(locations=list(zip(before['latitude'],before['longitude'])),color='blue').add_to(group_before)
+        mark_map(group_before,before,'fish','blue')
+        hm_group_before = folium.FeatureGroup(name = f"Indidvidual {name} HeatMap before event").add_to(map)
+        heatmap_before = plugins.HeatMap(data=before[['latitude', 'longitude']], radius=15).add_to(hm_group_before)
+
+    if not during.empty:
+        group_during = folium.FeatureGroup(name = f"Indidvidual {name} Trajectory during event").add_to(map)
+        # line_during = folium.PolyLine(locations=list(zip(during['latitude'],during['longitude'])),color='green').add_to(group_during)
+        mark_map(group_during,during,'fish','green')
+        hm_group_during = folium.FeatureGroup(name = f"Indidvidual {name} HeatMap during event").add_to(map)
+        heatmap_during = plugins.HeatMap(data=during[['latitude', 'longitude']], radius=15).add_to(hm_group_during)
+
+    if not after.empty:    
+        group_after = folium.FeatureGroup(name =  f"Indidvidual {name} Trajectory after event").add_to(map)
+        # line_after = folium.PolyLine(locations=list(zip(after['latitude'],after['longitude'])),color='red').add_to(group_after)
+        mark_map(group_after,after,'fish','red')
+        hm_group_after = folium.FeatureGroup(name =  f"Indidvidual {name} HeatMap after event").add_to(map)
+        heatmap_after = plugins.HeatMap(data=after[['latitude', 'longitude']], radius=15).add_to(hm_group_after)
+    
+    add_sea_depth(map) # Adds an image of the depth of the see based on 4 coordinates
+    folium.map.LayerControl(position='topright', collapsed=False).add_to(map)
+    MousePosition().add_to(map)
+
+    return map
+
+
+def add_sea_depth(map):
+    sea_depth_img = 'C:/Users/Ben/Documents/GitHub/Animal-Motion-for-predictions/data/Images/depth.png'
+    map_group = folium.FeatureGroup(name='Sea Depth Golf of Mexico').add_to(map)
+    image_overlay = folium.raster_layers.ImageOverlay(
+        image= sea_depth_img,
+        bounds=[[30.8936, -100.5469], [16.3037, -71.3672]],
+        opacity=0.5)
+    image_overlay.add_to(map_group)
+
+def make_heatMap(map, animals):
+    species = animals['species'].iloc[0]
+    hm_group = folium.FeatureGroup(name = f"Heat Map of {species}").add_to(map)
+    heatmap_during = plugins.HeatMap(data=animals[['latitude', 'longitude']], radius=15).add_to(hm_group)
+    MousePosition().add_to(map)
+    return map
+
+
+def make_polylines(map, animals):
+    i = len(color_list)-1
+    for shark_name, group in animals.groupby('name'):
+        feature_group = folium.FeatureGroup(name = shark_name).add_to(map)
+        line = folium.PolyLine(locations = list(zip(group['latitude'],group['longitude'])), color = color_list[i]).add_to(feature_group)
+        i = i-1
+        if i==-1:
+            i = len(color_list)-1
+    folium.map.LayerControl(position='topright', collapsed=False).add_to(map)
+    MousePosition().add_to(map)
+    return map
+
+def mark_map_multiple(map,animal, the_icon ='lion'):
+    color_index = len(color_list)-1
+    for shark_name, group in animal.groupby('name'):
+        feature_group = folium.FeatureGroup(name = shark_name).add_to(map)
+        mean_lat = group['latitude'].mean()
+        mean_lon = group['longitude'].mean()
+        for i in range(group.shape[0]):
+            lat = group['latitude'].iloc[i]
+            lon = group['longitude'].iloc[i]
+            date2 = group['date'].iloc[i]
+            name = group['name'].iloc[i]        
+            folium.Marker([lat, lon], popup = [name, date2], icon=folium.Icon(color=color_list[color_index],icon=the_icon,prefix='fa')).add_to(feature_group)
+            color_index = color_index-1
+            if color_index==-1:
+                color_index = len(color_list)-1
+    folium.map.LayerControl(position='topright', collapsed=False).add_to(map)
+    MousePosition().add_to(map)
+    return map
+
+
+def plot_model_Evaluation(model,y_test,y_pred,X_test,X):
+    fig, axs = plt.subplots(2, 2, figsize=(10, 8))
+
+    cm = confusion_matrix(y_test, y_pred)
+
+    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", ax=axs[0, 0])
+    axs[0, 0].set_title("Confusion Matrix")
+    axs[0, 0].set_xlabel("Predicted Labels")
+    axs[0, 0].set_ylabel("True Labels")
+
+    fpr, tpr, thresholds = roc_curve(y_test, y_pred)
+
+    roc_auc = roc_auc_score(y_test, y_pred)
+
+    axs[0, 1].plot(fpr, tpr, label=f"AUC = {roc_auc:.2f}")
+    axs[0, 1].plot([0, 1], [0, 1], "k--")
+    axs[0, 1].set_xlim([0.0, 1.0])
+    axs[0, 1].set_ylim([0.0, 1.05])
+    axs[0, 1].set_xlabel("False Positive Rate")
+    axs[0, 1].set_ylabel("True Positive Rate")
+    axs[0, 1].set_title("Receiver Operating Characteristic")
+    axs[0, 1].legend(loc="lower right")
+
+    precision, recall, thresholds = precision_recall_curve(y_test, y_pred)
+
+    axs[1, 0].plot(recall, precision)
+    axs[1, 0].set_xlim([0.0, 1.0])
+    axs[1, 0].set_ylim([0.0, 1.05])
+    axs[1, 0].set_xlabel("Recall")
+    axs[1, 0].set_ylabel("Precision")
+    axs[1, 0].set_title("Precision-Recall Curve")
+
+    result = permutation_importance(model, X_test, y_test, n_repeats=10, random_state=42)
+    sorted_idx = result.importances_mean.argsort()
+
+    axs[1, 1].barh(X.columns[sorted_idx], result.importances_mean[sorted_idx])
+    axs[1, 1].set_xlabel("Feature Importance")
+    axs[1, 1].set_title("Feature Importance")
+
+    plt.tight_layout()
+
+    plt.show()
+
+
--- src/preprocessor.py
+++ src/preprocessor.py
@@ -5,7 +5,7 @@ from rasterio.transform import Affine
 
 
 
-def sperm_whale_preprocess(path):
+def preprocess_sperm_whale(path):
     sperm_whales = pd.read_csv(path)
     temp_df = sperm_whales['event-id'].str.split(',', expand=True)
     temp_df.columns = ['event-id', 'visible', 'timestamp', 'location-long', 'location-lat',
@@ -27,13 +27,17 @@ def sperm_whale_preprocess(path):
     sperm_whales = sperm_whales.rename(columns={'timestamp': 'date'})
     sperm_whales = sperm_whales.rename(columns={'individual-local-identifier': 'name'})
     sperm_whales['date'] = pd.to_datetime(sperm_whales['date']) 
+    sperm_whales['year'] = sperm_whales['date'].dt.year
     sperm_whales['depth'] = sperm_whales.apply(lambda row: get_sea_depth(row['latitude'], row['longitude']),axis=1)
     sperm_whales['species'] = 'Physeter Macrocephalus'
+    sperm_whales = sperm_whales.drop('event-id',axis=1)
+    sperm_whales = sperm_whales.drop('visible',axis=1)
+
 
     return sperm_whales
 
 
-def blue_whale_preprocess(path):
+def preprocess_blue_whale(path):
     blue_whales = pd.read_csv(path)
     blue_whales =blue_whales.iloc[:,[0,1,2,3,4,18]].copy() # refine the csv and get only the desired columns from og dataset
     blue_whales = blue_whales.rename(columns={'location-long': 'longitude', 'location-lat': 'latitude', 'tag-local-identifier': 'name'})
@@ -48,48 +52,88 @@ def blue_whale_preprocess(path):
     blue_whales = blue_whales.rename(columns={'timestamp': 'date'})
     blue_whales = blue_whales.rename(columns={'individual-local-identifier': 'name'})
     blue_whales['date'] = pd.to_datetime(blue_whales['date'])
+    blue_whales['year'] = blue_whales['date'].dt.year
+
     blue_whales['depth'] = blue_whales.apply(lambda row: get_sea_depth(row['latitude'], row['longitude']),axis=1)
     blue_whales['species'] = 'Balaenoptera Musculus'
+    blue_whales = blue_whales.drop('event-id',axis=1)
+    blue_whales = blue_whales.drop('visible',axis=1)
+
 
     return blue_whales
 
 
-# def process_hurricane_txt(file_path):
-#     """
-#     This function reads the hurricane text file taken from the National Hurricane Center nhc.noaa.gov
-#     The only parameter is the text file path itself and the functions
-#     returns a pandas data frame with relevant columns in correct form.
 
-#     This link for data: https://www.nhc.noaa.gov/data/hurdat/hurdat2-1851-2020-052520.txt
-#     """
-#     hurricane_columns = ['date', 'Hour UTC', 'RI', 'HU', 'latitude', 'longitude', 'max_wind', 'min_pressure', 'ne34', 'se34', 'sw34', 'nw34', 'ne50', 'se50', 'sw50', 'nw50', 'ne64', 'se64', 'sw64', 'nw64', 'name']
+"""
+This function proccesses Tiger shark data from the DataONE website.
+It also adds the new columns:
+year - the year of observation
+depth - the water depth in meters where the animal was swimming
 
-#     # Read the text file line by line and store the names in a dict of names for easy access later
-#     hurricane_info = []
-#     current_hurricane = None
-#     names_dict = {}  # Keep track of hurricane names and suffixes
-#     with open(file_path, 'r') as f:
-#         for line in f:
-#             # if this is a new hurricane
-#             if line.startswith('AL'): # AL is a starter indicating new hurricane information
-#                 name = line.split(',')[1].strip() # get the name which is after the comma
-#                 suffix = names_dict.get(name, 1)  # Get the suffix for this name, default to 1
-#                 names_dict[name] = suffix + 1  # Increment suffix for this name and store it in the dictionary
-#                 if suffix > 1:
-#                     name = f"{name}{suffix}"  # Add suffix to the name if necessary
-#                     print('and is now changed to ', name, '\n')
-#                 row = {'name': name} # store the name
-#                 print(row)
-#                 current_hurricane = row
-#                 hurricane_info.append(row)
-#             # if this is a row of data for the current hurricane
-#             elif current_hurricane is not None:
-#                 row_data = line.strip().split(',')
-#                 row = {col: row_data[i].strip() for i, col in enumerate(hurricane_columns)}
-#                 print(row[name])
-#                 current_hurricane.update(row)
-#     return pd.DataFrame(hurricane_info)
+Data Acknowledgements: 
 
+Movement Patterns and Habitat Use of Tiger Sharks Across Ontogeny in the Gulf of Mexico, 2010-2018
+Published in 2020 and 
+written by Matthew Ajemian, Neil Hammerschlag, Marcus Drymon, David Wells, and Brett Falterman
+https://search.dataone.org/view/10.24431/rw1k44e
+
+"""
+def preprocess_tiger_sharks(path):    
+    tiger_sharks = pd.read_csv(path)
+    tiger_sharks['datetime '] = pd.to_datetime(tiger_sharks['datetime '])
+    tiger_sharks = tiger_sharks.rename(columns = {'datetime ':'date', 
+                                                  'lon':'longitude', 
+                                                  'lat': 'latitude',
+                                                  'id':'name'})
+    tiger_sharks['year'] = tiger_sharks['date'].dt.year
+    tiger_sharks['depth'] = tiger_sharks.apply(lambda row: get_sea_depth(row['latitude'], row['longitude']),axis=1)
+    tiger_sharks['species'] = 'Galeocerdo Cuvier'
+    tiger_sharks = tiger_sharks.drop('lc',axis=1)
+    tiger_sharks = tiger_sharks[['date', 'longitude', 'latitude', 'name', 'year', 'depth', 'species']]
+
+    return tiger_sharks
+
+
+def preprocess_WA_sharks(path):    
+    WA_sharks = pd.read_csv(path)
+    WA_sharks['Date'] = pd.to_datetime(WA_sharks['Date'])
+    WA_sharks = WA_sharks.rename(columns = {'Date':'date', 
+                                            'Latitude_DD':'longitude', 
+                                            'Longitude_DD': 'latitude',
+                                            'RotoTag':'name',
+                                            'Species':'species'})
+
+    WA_sharks['year'] = WA_sharks['date'].dt.year
+    WA_sharks['depth'] = WA_sharks.apply(lambda row: get_sea_depth(row['latitude'], row['longitude']),axis=1)
+    WA_sharks = WA_sharks.drop(['Sex','Cal','TL','TL_Notes','FL','PCL','SpaghettiTag','Notes'],axis=1)
+    WA_sharks = WA_sharks[['date', 'longitude', 'latitude', 'name', 'year', 'depth', 'species']]
+
+    return WA_sharks
+
+def preprocess_whale_sharks(path):
+    whale_sharks = pd.read_csv(path)
+    for i, row in whale_sharks.iterrows():
+            whale_sharks.at[i, "longitude"] = float(row["longitude"])
+            whale_sharks.at[i, "latitude"] = float(row["latitude"])
+
+    whale_sharks = whale_sharks.rename(columns={'timestamp': 'date'})
+    whale_sharks = whale_sharks.rename(columns={'id_tag': 'name'})
+    whale_sharks['date'] = pd.to_datetime(whale_sharks['date'])
+    whale_sharks = whale_sharks.drop('month' , axis=1)
+    whale_sharks['year'] = whale_sharks['date'].dt.year
+
+    whale_sharks['depth'] = whale_sharks.apply(lambda row: get_sea_depth(row['latitude'], row['longitude']),axis=1)
+    whale_sharks['species'] = 'Whale Sharks Rhincodon Typus'
+    whale_sharks = whale_sharks.drop('event-id',axis=1)
+    whale_sharks = whale_sharks.drop('visible',axis=1)
+
+
+    return whale_sharks
+
+
+
+###################################### Weather Data ################################################3    
+        
 
 def preProcess_weather_data(data):
     """
@@ -124,7 +168,7 @@ def preProcess_weather_data(data):
     # cols = list(data.columns)
     # cols.append(cols.pop(cols.index('name')))
     # data = data[cols]
-    data['year'] = data['date'].dt.year 
+    data['year'] = data['date'].dt.year
 
     return data
 
@@ -171,117 +215,11 @@ def process_hurricane_txt(file_path):
     processed_df = preProcess_weather_data(processed_df)
     return processed_df
 
-# def proccess_hurricane_txt(file_path):
-    
-#     """
-#     This function reads the hurrcane text file taken from the National Hurricane Center nhc.noaa.gov
-#     The only parameter is the text file path itself and the functions
-#     returns a pandas data frame with relevant columns in correct form.
-
-#     This link for data: https://www.nhc.noaa.gov/data/hurdat/hurdat2-1851-2022-042723.txt
-#     """
-#     hurricane_columns = ['date', 'Hour UTC', 'RI', 'HU', 'latitude', 'longitude', 'max_wind', 'min_pressure', 'ne34', 'se34', 'sw34', 'nw34', 'ne50', 'se50', 'sw50', 'nw50', 'ne64', 'se64', 'sw64', 'nw64', 'name']
-    
-#     # Read the text file line by line and store the names in a dict of names for easy access later
-#     hurricane_info = []
-#     current_hurricane = None
-#     with open(file_path, 'r') as f:
-#         for line in f:
-#             # if this is a new hurricane
-#             if line.startswith('AL'): # AL is a starter indicating new hurricane information
-#                 name = line.split(',')[1].strip() # get the name which is after the coma
-#                 # row = {'name': name} # store the name
-#                 current_hurricane = name
-
-#             else:
-#                 values = line.strip().split(',') # this is information so split it into columns based on the column list
-#                 # row.update(zip(hurricane_columns[:-1], values)) # use all columns besides the last one which is a new column called name
-#                 row = dict(zip(hurricane_columns, values))
-#                 row['name'] = current_hurricane
-#                 hurricane_info.append(row)
-
-#     processed_df = pd.DataFrame(hurricane_info, columns= hurricane_columns)
-#     processed_df =processed_df.iloc[:,[0,1,4,5,6,7,20]].copy()
-#     processed_df = preProcess_weather_data(processed_df)
-#     processed_df['year'] = processed_df['date'].dt.year 
-#     return processed_df
-
-
-# def preProcess_weather_data(data):
-#     """
-#     The weather data contains coordinates in the format of N and W. eg: 32.43N and 23.3W
-#     This function makes all N & E positive and S & W negative 
-#     """
-    
-#     if isinstance(data['date'].iloc[0], str):
-#         data['date'] = data['date'].apply(lambda x: pd.to_datetime(str(x), format='%Y%m%d'))
         
-#     lat_column = data['latitude']
-#     lon_column = data['longitude']
 
-#     # A loop that checks the latitude
-#     for i in range(len(lat_column)):
-#         value = lat_column.iloc[i]
-#         if "N" in value:
-#             lat = float(value.strip("N"))
-#         elif "S" in value:
-#             lat = -1 * float(value.strip("S"))
-#         lat_column.iloc[i] = lat
-#     # A loop that checks the longitude
-#     for i in range(len(lon_column)):
-#         value = lon_column.iloc[i]
-#         if "E" in value:
-#             lon = float(value.strip("E"))
-#         elif "W" in value:
-#             lon = -1 * float(value.strip("W"))
-#         lon_column.iloc[i] = lon
-#     data['latitude'] = lat_column
-#     data['longitude'] = lon_column
-#     return data
+    ########################################### General #################################################
 
-
-"""
-This function proccesses Tiger shark data from the DataONE website.
-It also adds the new columns:
-year - the year of observation
-depth - the water depth in meters where the animal was swimming
-
-Data Acknowledgements: 
-
-Movement Patterns and Habitat Use of Tiger Sharks Across Ontogeny in the Gulf of Mexico, 2010-2018
-Published in 2020 and 
-written by Matthew Ajemian, Neil Hammerschlag, Marcus Drymon, David Wells, and Brett Falterman
-https://search.dataone.org/view/10.24431/rw1k44e
-
-"""
-def preprocess_tiger_sharks(path):    
-    tiger_sharks = pd.read_csv(path)
-    tiger_sharks['datetime '] = pd.to_datetime(tiger_sharks['datetime '])
-    tiger_sharks = tiger_sharks.rename(columns = {'datetime ':'date', 
-                                                  'lon':'longitude', 
-                                                  'lat': 'latitude',
-                                                  'id':'name'})
-    tiger_sharks['year'] = tiger_sharks['date'].dt.year
-    tiger_sharks['depth'] = tiger_sharks.apply(lambda row: get_sea_depth(row['latitude'], row['longitude']),axis=1)
-
-    return tiger_sharks
-
-def preprocess_whale_sharks(path):
-    whale_sharks = pd.read_csv(path)
-    for i, row in whale_sharks.iterrows():
-            whale_sharks.at[i, "longitude"] = float(row["longitude"])
-            whale_sharks.at[i, "latitude"] = float(row["latitude"])
-
-    whale_sharks = whale_sharks.rename(columns={'timestamp': 'date'})
-    whale_sharks = whale_sharks.rename(columns={'id_tag': 'name'})
-    whale_sharks['date'] = pd.to_datetime(whale_sharks['date'])
-    whale_sharks = whale_sharks.drop('month' , axis=1)
-    whale_sharks['depth'] = whale_sharks.apply(lambda row: get_sea_depth(row['latitude'], row['longitude']),axis=1)
-    whale_sharks['species'] = 'Rhincodon Typus'
-
-    return whale_sharks
-
-"""
+    """
 This calculates the depth of the water given <lat, lon> values.
 This done using the sea_depth.tiff file.
 Extracted from:
@@ -318,7 +256,6 @@ def make_cp_dict(g_p, a_data, full_hurricane_1992):
                                     }
             
         for animal_na in curernt_event_group['name'].unique():
-            print(animal_na)
             animal_data = a_data[a_data['name']==animal_na]
             animal_data = animal_data.sort_values('date')
             animal_before = animal_data.loc[(animal_data['date'] < event_start_date) & (animal_data['date'] >= event_start_date - pd.Timedelta(days=7))]
@@ -335,13 +272,23 @@ def make_cp_dict(g_p, a_data, full_hurricane_1992):
             
     return Crossed_points
 
+def preprocess_4_LR(path):
 
+    t = pd.read_csv(path)
+
+    t['Barometric Pressure'] = t['Barometric Pressure'].replace('-', float('nan'))
+    t['Wind Speed (kn)'] = t['Wind Speed (kn)'].replace('-', float('nan'))
+    t['Air Temp (°F)'] = t['Air Temp (°F)'].replace('-', float('nan'))
+    t['Wind Gust (kn)'] = t['Wind Gust (kn)'].replace('-', float('nan'))
+    t['Humidity (%)'] = t['Humidity (%)'].replace('-', float('nan'))
+
+    t['Barometric Pressure'] = t['Barometric Pressure'].astype(float)
+    t['Wind Speed (kn)'] = t['Wind Speed (kn)'].astype(float)
+    t['Air Temp (°F)'] = t['Air Temp (°F)'].astype(float)
+    t['Wind Gust (kn)'] = t['Wind Gust (kn)'].astype(float)
+    t['Humidity (%)'] = t['Humidity (%)'].astype(float)
     
-        
-
-        
-
-    
+    return t
 
 
 
